7c41bb72d9e020e6f24855940d8636d7582b9027
Roy Straub <roy@codecraftr.nl>
2023-07-16 16:16:04 +0200

    Artificial delay

--- src/main/scala/nl/codecraftr/scala/kata/ApiRateProvider.scala
+++ src/main/scala/nl/codecraftr/scala/kata/ApiRateProvider.scala
@@ -2,9 +2,14 @@ package nl.codecraftr.scala.kata
 
 // Imagine this is an external API
 object ApiRateProvider {
-  def rateProvider(stockType: StockType): Double = stockType match {
-    case Petroleum => 3.99
-    case Dollar    => 1.23
-    case Gold      => 101.73
+  def rateProvider(stockType: StockType): Double = {
+    // Imagine this being a slow api call
+    Thread.sleep(250)
+
+    stockType match {
+      case Petroleum => 3.99
+      case Dollar    => 1.23
+      case Gold      => 101.73
+    }
   }
 }
